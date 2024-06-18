7c7008cfcac464ad27d9f0b2e43e5457c25b624e
yangaeyaoung <aeyooung0325@naver.com>
2024-06-18 14:33:17 +0900

    setting react blog

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  let post = '우우 재미없다.';
+  let [a, b] = useState('유잼 운동 추천');
+
+  return (
+    <div className="App">
+      <div class="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <div class="list">
+        <h4>{ a }</h4>
+        <p>6월 17일 발행</p>
+      </div>
+      <h4>{ post }</h4>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
