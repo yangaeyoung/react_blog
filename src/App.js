dfecacbf710f95177188c85c7abea61d02955f38
yangaeyoung <aeyoung0325@naver.com>
2024-06-18 17:29:52 +0900

    useState_copy

--- src/App.js
+++ src/App.js
@@ -4,20 +4,42 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = '우우 재미없다.';
-  let [a, b] = useState('유잼 운동 추천');
+  let [글제목, 글제목변경] = useState(['커피 오늘만 몇 잔째?', '우우 재미없다.', '유잼 운동 추천']);
+  let [따봉, 따봉변경] = useState(0);
+
+  // function like(){
+  //   console.log(1);
+  // }
+
 
   return (
     <div className="App">
-      <div class="black-nav">
-        <h4>블로그임</h4>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
       </div>
-      <div class="list">
-        <h4>{ a }</h4>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      }}>가나다순정렬</button>
+
+      <div className="list">
+        <h4>{ 글제목[0] } <button onClick={()=>{
+          let copy = [...글제목];
+          copy[0] = '노잼 운동 추천';
+          글제목변경(copy);
+      }}>↔️</button></h4>
         <p>발행일: 6월 18일</p>
       </div>
-      <h4>{ post }</h4>
-      <p></p>
+      <div className="list">
+        <h4>{ 글제목[1] } <span onClick={()=>{ 따봉변경(따봉+1) } }>👍</span> { 따봉 } </h4>
+        <p>발행일: 6월 17일</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>발행일: 6월 16일</p>
+      </div>
     </div>
   );
 }
